0f924c12af673f3d3da41b927c55aa35fb658364
Somnath Baidya <84554527+dessomu@users.noreply.github.com>
2024-07-05 09:16:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let [userDetails, setUserDetails] = useState([])
+  let [userDetails, setUserDetails] = useState([]);
 
-  let [userName,setUserName]=useState("")
+  let [userName,setUserName] = useState("");
 
-  let [userEmail,setUserEmail]=useState("")
+  let [userEmail,setUserEmail] = useState("");
 
-  let [userAge,setUserAge]=useState("")
+  let [userAge,setUserAge] = useState("");
+
+  let [isModalOpen, setIsModalOpen] = useState(false);
+
+  let [selectedUserId, setSelectedUserId] = useState("");
+
+  let [updatedUserName,setUpdatedUserName] = useState("");
+
+  let [updatedUserEmail,setUpdatedUserEmail] = useState("");
+
+  let [updatedUserAge,setUpdatedUserAge] = useState("");
 
   function userDetailHandler(e){
-    const id=parseInt(Math.ceil(Math.random()*1000))
+    const id=Math.ceil(Math.random()*1000).toString();
     const storeUserObj={};
     storeUserObj.id=id;
     storeUserObj.name=userName;
@@ -40,36 +50,37 @@ function App() {
     const updateUserDetails=userDetails.filter((userDetail)=>userDetail.id !== parseInt(id));
     setUserDetails(updateUserDetails);
   }
-  // function editUserHandler(id){
-  //   // const idHolder=id;
-  //   // console.log(idHolder);
-  //   return(
-  //     <div  className="modal">
-  //          {
-  //            userDetails.map(userDetail=>{
-  //             if (userDetail.id === id) {
-  //               return (
-  //                 <div key={userDetail.id} className="inner-modal">
-  //                     <label  htmlFor="edit-name">Edit Name</label>
-  //                     <input value={userName} onChange={(e)=>setUserName(e.target.value)} id="edit-name" type="text" placeholder="Edit Name"/>
-  //                     <label htmlFor="edit-email">Edit Email</label>
-  //                     <input value={userEmail}  onChange={(e)=>setUserEmail(e.target.value)} id="edit-email" type="text" placeholder="Edit Email"/>
-  //                     <label htmlFor="edit-age">Edit Age</label>
-  //                     <input value={userAge}  onChange={(e)=>setUserAge(e.target.value)} id="edit-age" type="text" placeholder="Edit Age"/>
-  //                     <button className="cancel">Cancel</button>
-  //                     <button className="update">Update</button>
-  //                 </div>
-  //               )
-  //             }
-  //             else{
-  //               return userDetails;
-  //             };
-  //             })
-  //           }
-      
-  //     </div>
-  //   )
-  // }
+
+
+  function editUserHandler(){
+    setSelectedUserId(userDetail.id);
+    setUpdatedUserName(userDetail.name);
+    setUpdatedUserEmail(userDetail.email);
+    setUpdatedUserAge(userDetail.age);
+    setIsModalOpen(true);  
+  }
+
+  function closeModal(){
+    setIsModalOpen(false);
+  }  
+
+ function updateUserHandler(){
+    const updatedUserDetails = userDetails.map( (userDetail) => {
+        if( userDetail.id === selectedUserId ){
+            return {
+                ...userDetail,
+                name : updatedUserName,
+                email : updatedUserEmail,
+                age : updatedUserAge
+            }
+        }else{
+            return userDetail;
+        };
+    setUserDetails(updatedUserDetails);
+    closeModal();
+        
+    })
+  }  
 
   return (
     <>
@@ -113,17 +124,30 @@ function App() {
               <p>Name: <span>{userDetail.name}</span></p>
               <p>Email: <span>{userDetail.email}</span></p>
               <p>Age: <span>{userDetail.age}</span></p>
-              <button  className="edit">Edit</button>
+              <button onClick={()=>editUserHandler(userDetail)}  className="edit">Edit</button>
               <button onClick={()=>deleteUserHandler(userDetail.id)} className="delete">Delete</button>
               {/* <h1 style={{color:"white"}}>{userDetail.id}</h1> */}
            </div>
-           
+         
             
           );
         })
         }
        
       </div>
+      {isModalOpen ?   <div  className="modal">
+                  <div  className="inner-modal">
+                      <label  htmlFor="edit-name">Edit Name</label>
+                      <input value={updatedUserName} onChange={(e)=>setUpdatedUserName(e.target.value)} id="edit-name" type="text" placeholder="Edit Name"/>
+                      <label htmlFor="edit-email">Edit Email</label>
+                      <input value={updatedUserEmail}  onChange={(e)=>setUpdatedUserEmail(e.target.value)} id="edit-email" type="text" placeholder="Edit Email"/>
+                      <label htmlFor="edit-age">Edit Age</label>
+                      <input value={updatedUserAge}  onChange={(e)=>setUpdatedUserAge(e.target.value)} id="edit-age" type="text" placeholder="Edit Age"/>
+                      <button onClick={closeModal} className="cancel">Cancel</button>
+                      <button onClick={updateUserHandler} className="update">Update</button>
+                  </div>
+           </div> : null}    
+      
     
     </>
   );
